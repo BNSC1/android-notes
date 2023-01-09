9c81180ec6ce81e4c7a7185a30ba18687e34965c
B.N <19542357+BNSC1@users.noreply.github.com>
2023-01-09 15:31:13 +0800

    - Update: replace fibonacci DP cache to array

--- code/algorithm/fibonacci_iterative_dp.kt
+++ code/algorithm/fibonacci_iterative_dp.kt
@@ -1,10 +1,10 @@
 fun fibonacci(num: Int): Int {
     val n = abs(num) //absolute number for index and size
-    val cache = ArrayList<Int>()
-    cache.add(0)
-    cache.add(1)
+    val cache = IntArray((n+1).coerceAtLeast(2)) { -1 } //cache should have at least the size of 2, to contain base cases, -1 to indicate uninitialized
+    cache[0] = 0
+    cache[1] = 1
     for (i in 2 .. n) {
-        cache.add(i,cache[i-1] + cache[i-2])
+        cache[i] = cache[i-1] + cache[i-2]
     }
     return if (num % 2 == 0 && num < 0) { //fibonacci result will be negative when the input is even and is negative
         -cache[n]
--- code/algorithm/fibonacci_recursive_dp.kt
+++ code/algorithm/fibonacci_recursive_dp.kt
@@ -1,20 +1,18 @@
 fun fibonacci(num: Int): Int {
     val n = abs(num) //absolute number for index and size
-    val cache = ArrayList<Int>()
-    cache.add(0)
-    cache.add(1)
+    val cache = IntArray((n+1).coerceAtLeast(2)) { -1 } //cache should have at least the size of 2, to contain base cases, -1 to indicate uninitialized
+    cache[0] = 0
+    cache[1] = 1
     return if (num % 2 == 0 && num < 0) { //fibonacci result will be negative when the input is even and is negative
         -cacheOrCalculate(n, cache)
-    }
-    else {
+    } else {
         cacheOrCalculate(n, cache)
     }
 }
 
-fun cacheOrCalculate(n: Int, cache: ArrayList<Int>): Int {
-    if (cache.size <= n) {
-        cache.add(n,
-                  cacheOrCalculate(n-2,cache) + cacheOrCalculate(n-1,cache)) //add to cache if not exist yet
+fun cacheOrCalculate(n: Int, cache: IntArray): Int {
+    if (cache[n] == -1) {
+        cache[n] = cacheOrCalculate(n-2,cache) + cacheOrCalculate(n-1,cache) //add to cache if not exist yet
     }
     return cache[n]
 }
