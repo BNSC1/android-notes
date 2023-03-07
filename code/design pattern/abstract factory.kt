c69f2b7d89882821fff7e6dd6e6fddafa3dbe15a
B.N <19542357+BNSC1@users.noreply.github.com>
2023-03-07 11:35:13 +0800

    - Update: abstract factory

--- code/design pattern/abstract factory.kt
+++ code/design pattern/abstract factory.kt
@@ -1,21 +1,11 @@
-class Main {
-    lateinit var factory: Factory //flavor unknown until init
-    @Test
-    fun main() {
-        factory = when (FLAVOR) {
-            "Gaming" -> GamingFactory()
-            "Office" -> OfficeFactory()
-            else -> throw IllegalStateException("Unknown flavor!")
-        }
-        factory.makeMouse() `should be instance of` GamingMouse::class
-        factory.makeKeyboard() `should be instance of` GamingKeyboard::class //got gaming mouse/keyboard
-    }
-    companion object {
-        const val FLAVOR = "Gaming"
+//https://refactoring.guru/design-patterns/abstract-factory
+/* 
+1. Map out a matrix of distinct product types versus variants of these products.
+*/
 
-    }
-}
-
+/* 
+2. Declare abstract product interfaces for all product types. Then make all concrete product classes implement these interfaces.
+*/
 interface Mouse
 
 class GamingMouse: Mouse
@@ -28,11 +18,17 @@ class GamingKeyboard: Keyboard
 
 class OfficeKeyboard: Keyboard
 
+/* 
+3. Declare the abstract factory interface with a set of creation methods for all abstract products.
+*/
 interface Factory {
     abstract fun makeKeyboard(): Keyboard
     abstract fun makeMouse(): Mouse
 }
 
+/* 
+4. Implement a set of concrete factory classes, one for each product variant.
+*/
 class GamingFactory: Factory{
     override fun makeKeyboard() = GamingKeyboard()
     override fun makeMouse() = GamingMouse()
@@ -41,4 +37,31 @@ class GamingFactory: Factory{
 class OfficeFactory: Factory {
     override fun makeKeyboard() = OfficeKeyboard()
     override fun makeMouse() = OfficeMouse()
+}
+
+class Main {
+    lateinit var factory: Factory //flavor unknown until init
+    @Test
+    fun main() {
+        /* 
+        5. Create factory initialization code somewhere in the app.
+           It should instantiate one of the concrete factory classes, depending on the application configuration or the current environment.
+           Pass this factory object to all classes that construct products.
+        */
+        /* 
+        6. Scan through the code and find all direct calls to product constructors.
+           Replace them with calls to the appropriate creation method on the factory object.
+        */
+        factory = when (FLAVOR) {
+            "Gaming" -> GamingFactory()
+            "Office" -> OfficeFactory()
+            else -> throw IllegalStateException("Unknown flavor!")
+        }
+        factory.makeMouse() `should be instance of` GamingMouse::class
+        factory.makeKeyboard() `should be instance of` GamingKeyboard::class //got gaming mouse/keyboard
+    }
+    companion object {
+        const val FLAVOR = "Gaming"
+
+    }
 }
